adcd5145402c628a2348adc1b2a6921bf949db7d
TyJaYo <tylerhotmail@hotmail.com>
2018-07-08 02:51:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,6 @@
 def space; "   "; end
 def pipe; "|"; end
 def line; "-----------\n"; end
-def row; space + pipe + space + pipe + space; + "\n" end
+def row; space + pipe + space + pipe + space + "\n"; end
 def board; row + line + row + line + row; end
 def display_board; print board; end
